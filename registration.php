c7aba6a97e9c4d61217a9d6564b67cbb61341b9e
nikafacia <108878977+nikafacia@users.noreply.github.com>
2022-07-07 17:24:32 +0400

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,36 @@
+ <!DOCTYPE html>
+<html 
+<head>
+	<title>registration</title>
+</head>
+<body>
+
+	   <style type="text/css"
+
+       #text{
+
+       	  height: 25pxp;
+       	  bordier-radius: 5pxp;
+       	  padding :4pxp;
+
+       	  header(Location: website.html");
+
+       	  <!DOCTYPE html>
+<html lang="geo">
+<head>
+    <meta charset="UTF-8">
+    <title>title>
+    <link
+    <link 
+</head>
+<body>
+    <div class="full-page">
+        <div class='navbar'>
+            <div class='logo'>
+                <a href='#'><h1>ქართული</h1></a>
+            </div>
+            <nav>
+                <ul id='MenuItems'>
+                    <li><a href='#'>ყველაფერი</a></li>
+                    <li><a href='#'>შესახებ</a></li>
+                    <li><a href='#'>პაკეტები</a></li>
